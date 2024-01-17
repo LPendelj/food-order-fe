0a1b86b5b9603fb720c2d4d031bd5c0c45a9dcf0
Luka Pendelj <pendeljluka@gmail.com>
2024-01-17 18:40:10 +0100

    Created default styles for buttons and text inputs

--- src/app/components/partials/text-input/text-input.component.ts
+++ src/app/components/partials/text-input/text-input.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-text-input',
+  selector: 'text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss']
 })
-export class TextInputComponent {
+export class TextInputComponent implements OnInit {
+
+
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorWhen: boolean = true;
+
+  @Input()
+  label!: string;
+  @Input()
+  type: 'text' | 'password' | 'email' = 'text';
+
+  get formControl(){
+    return this.control as FormControl
+  }
+
+  constructor(){
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
 
 }
