c9d02d32b1c78eda60236f509bb4c67ce5330f7d
Luka Pendelj <pendeljluka@gmail.com>
2025-04-04 20:17:57 +0100

    UI changes - Changed  button styles

--- src/app/components/partials/default-button/default-button.component.ts
+++ src/app/components/partials/default-button/default-button.component.ts
@@ -12,7 +12,7 @@ export class DefaultButtonComponent implements OnInit{
   @Input()
   text: string = 'Submit';
   @Input()
-  bgColor= '#e72929'
+  bgColor= '#004E64'
   @Input()
   color= 'white'
   @Input()
